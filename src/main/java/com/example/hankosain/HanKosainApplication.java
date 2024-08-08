02561ab785ce106934017599b6ba455175225beb
ruukr8080@gmail.com <ruukr8080@gmail.com>
2024-08-08 17:36:52 +0900

    first commit

---
+++ src/main/java/com/example/hankosain/HanKosainApplication.java
@@ -0,0 +1,13 @@
+package com.example.hankosain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanKosainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanKosainApplication.class, args);
+    }
+
+}
